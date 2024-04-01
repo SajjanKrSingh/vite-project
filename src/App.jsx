3dfbea24820bd5cf11bfa3e50e352855664a7d15
sajjan kumar singh <sajjansingh1838@gmail.com>
2024-04-01 22:14:21 +0530

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState } from "react";
 
 const App = () => {
+
+  const [time, settime] = useState(new Date().toLocaleDateString());
+  // setInterval(() => {
+  //   settime(new Date().toLocaleTimeString())
+  // }, 1000);
+ 
+  const [username,setUsername]= useState('sajjan');
+//  const updateUsername = (x)=>{
+//    setUsername(x);
+//  }
   return (
     <>
-    <div>App</div>
+      <h1>{time}</h1>
+      <h1>{username}</h1>
+      <button onClick={()=> setUsername('max')}>fhfhfh</button>
+      <div>App</div>
     </>
   )
 }
